07a709d31705e4875509717ba749c5d79b6a4da4
Tanguy Palmie <tanguy.palmie@gmail.com>
2021-07-15 15:48:42 +0200

    Initial commit

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ const http = require('http');
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const routes = require("./db/route")
+//const routes = require("./db/route")
 const app = express();
 
 
 // Set static folder (middleware)
 app.use(express.static(path.join(__dirname, "frontend")));
 
-
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 30;
 
-app.use(bodyParser.json())
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-)
-routes(app)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//routes(app)
 
 
 app.listen(PORT, () => {
